ac060e967e6f7476d32a9820cf4704570269acb5
Mike Pryor <83245259+Mike-Pryor@users.noreply.github.com>
2022-01-11 19:03:39 -0500

    Just something to play around with.

---
+++ script.js
@@ -0,0 +1,5 @@
+const btn = document.querySelector("[data-btn]")
+btn.addEventListener("click", () => {
+  btn.classList.toggle("animating")
+})
+// In line 3, if you change "toggle" to "add" - will only animate once. Would use "add" in real-world production site.
